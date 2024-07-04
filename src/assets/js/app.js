471dd2a83231f58437ef719b5b7af8351f7e2362
Jona <jonathan.diaz@driv.in>
2024-07-04 13:07:14 -0400

    fix app

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,7 +7,7 @@ const finishedContainer = document.querySelector('.finished');
 let taskList = [];
 
 const addTask = (title) => {
-  if (!title || title.length == 0) return
+  if (!title || title.length == 0) return;
   const id = Math.floor(Math.random() * 9000) + 1000;
   const task = {
     id: id,
@@ -40,59 +40,63 @@ const addTask = (title) => {
     `;
 
   taskContainer.insertAdjacentHTML('beforeend', row);
-  taskInput.value = ''
-  const checkContainer = document.querySelector(`#check-${id}`)
-  const deleteContainer = document.querySelector(`#delete-${id}`)
-  checkContainer.style.backgroundImage = 'url("src/assets/img/processing.png")'
-  deleteContainer.style.backgroundImage = 'url("src/assets/img/delete.png")'
-  calculateStats()
+  taskInput.value = '';
+  const checkContainer = document.querySelector(`#check-${id}`);
+  const deleteContainer = document.querySelector(`#delete-${id}`);
+  checkContainer.style.backgroundImage = 'url("src/assets/img/processing.png")';
+  deleteContainer.style.backgroundImage = 'url("src/assets/img/delete.png")';
+  calculateStats();
 };
 
 const deleteTask = (id) => {
   taskList = taskList.filter((task) => task.id != id);
   document.querySelector(`#task-${id}`).remove();
-  calculateStats()
+  calculateStats();
 };
 
 const finishTask = (id, inputId) => {
-  const checked = document.querySelector(inputId).checked
-  const checkContainer = document.querySelector(`#check-${id}`)
-  const taskRow = document.querySelector(`#task-${id}`)
-  const small = document.querySelector(`#small-${id}`)
-  let task = taskList.find(task => task.id == id)
+  const checked = document.querySelector(inputId).checked;
+  const checkContainer = document.querySelector(`#check-${id}`);
+  const taskRow = document.querySelector(`#task-${id}`);
+  const small = document.querySelector(`#small-${id}`);
+  let task = taskList.find((task) => task.id == id);
 
   if (task && checked != null) {
-    task.finished = checked
-    small.textContent = checked 
-      ? 'Finalizada'
-      : 'Pendiente'
-
+    task.finished = checked;
+    small.textContent = checked ? 'Finalizada' : 'Pendiente';
     taskRow.style.outline = checked
       ? '1px solid rgba(40,201,55,1)'
-      : '1px solid rgba(255,0,0,0.5)'
+      : '1px solid rgba(255,0,0,0.5)';
 
     taskRow.style.background = checked
       ? 'rgba(40,201,55,0.1)'
-      : 'rgba(255,0,0,0.1)'
+      : 'rgba(255,0,0,0.1)';
 
     checkContainer.style.backgroundImage = checked
       ? 'url("src/assets/img/check.png")'
-      : 'url("src/assets/img/processing.png")'
+      : 'url("src/assets/img/processing.png")';
   }
-  calculateStats()
-}
+  calculateStats();
+};
 
 const calculateStats = () => {
-  totalContainer.textContent = `Total: ${taskList.length}`
-  pendingContainer.textContent = `Pendientes ${[...taskList].filter(task => !task.finished).length}` 
-  finishedContainer.textContent = `Finalizadas: ${[...taskList].filter(task => task.finished).length}`
-  taskInput.value = ''
-}
+  totalContainer.textContent = `Total: ${taskList.length}`;
+  pendingContainer.textContent = `Pendientes ${
+    [...taskList].filter((task) => !task.finished).length
+  }`;
+  finishedContainer.textContent = `Finalizadas: ${
+    [...taskList].filter((task) => task.finished).length
+  }`;
+  taskInput.value = '';
+  document.querySelector('.empty').style.display =
+    taskList.length == 0 ? 'grid' : 'none';
+};
+
+// Listeners
 addButton.addEventListener('click', () => addTask(taskInput.value));
 taskInput.addEventListener('keydown', (event) => {
   const keycode = event.keyCode || event.which;
-  if (keycode == 13) addTask(taskInput.value)
+  if (keycode == 13) addTask(taskInput.value);
 });
 
-
-window.taskList = taskList
+window.taskList = taskList;
